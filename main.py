2fdc80c6de43096972f36a2353dc660185a1f5a7
TahaSait <148285176+TahaSait@users.noreply.github.com>
2023-11-04 17:48:44 -0600

    Set up a dictonary to store instinces of numbers

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 sample_File = open("sample.ini")
+numbers_0_9 = {
+    '0': 0,
+    '1': 0,
+    '2': 0,
+    '3': 0,
+    '4': 0,
+    '5': 0,
+    '6': 0,
+    '7': 0,
+    '8': 0,
+    '9': 0,
+}
+for line in sample_File:
+    print(sample_File)
 read_file = sample_File.read()
 print(read_file)
