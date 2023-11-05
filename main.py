f7f046de9e33c3e488392f9dc62258c25045b884
TahaSait <148285176+TahaSait@users.noreply.github.com>
2023-11-04 18:23:55 -0600

    Changed varible names and added a vowel counter

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 sample_File = open("sample.ini")
 read_file = sample_File.read()
-number_0_to_9 = 0
-
+number_Counter = 0
+vowel_Counter = 0
 for char in read_file:
     if char.isdigit():
-        number_0_to_9 += 1
-print(f"total number count is {number_0_to_9}")
+        number_Counter += 1
+    if char in ["a","e","i","o","u"]:
+        vowel_Counter += 1
+
+
+print(f"total number count is {number_Counter}")
+print(f"total number count is {vowel_Counter}")
