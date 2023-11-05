3aa54627901e395f55f31b476ff1df52d9c80981
TahaSait <148285176+TahaSait@users.noreply.github.com>
2023-11-04 18:11:40 -0600

    got rid of dictionary and setup number counter

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 sample_File = open("sample.ini")
-numbers_0_9 = {
-    '0': 0,
-    '1': 0,
-    '2': 0,
-    '3': 0,
-    '4': 0,
-    '5': 0,
-    '6': 0,
-    '7': 0,
-    '8': 0,
-    '9': 0,
-}
-for line in sample_File:
-    print(sample_File)
 read_file = sample_File.read()
-print(read_file)
+number_0_to_9 = 0
+
+for char in read_file:
+    if char.isdigit():
+        number_0_to_9 += 1
+print(f"total number count is {number_0_to_9}")
